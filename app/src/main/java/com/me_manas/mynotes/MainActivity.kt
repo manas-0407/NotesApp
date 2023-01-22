ccf1179ed94c204d27823d3e8d3e19ad6aa50b68
manas <manasrai1234567@gmail.com>
2023-01-22 17:55:58 +0530

    Updated item.xml

--- app/src/main/java/com/me_manas/mynotes/MainActivity.kt
+++ app/src/main/java/com/me_manas/mynotes/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.me_manas.mynotes
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.switchmaterial.SwitchMaterial
 import java.util.*
 
@@ -18,36 +23,38 @@ import java.util.*
 class MainActivity : AppCompatActivity(), InterNotesRVAdapt {
 
     lateinit var viewModel: NoteViewModel
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpDrawerLayout()
 
-        //switch button
-        val switch : SwitchMaterial = findViewById(R.id.nightswitch)
-//        Objects.requireNonNull(getSupportActionBar())?.setTitle("Light-Night MODE SWITCH")
-
-        switch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
-            // do something, the isChecked will be
-            // true if the switch is in the On position
-            if(isChecked){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                buttonView.setText("NIGHT MODE")
-            }
-            else{
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                buttonView.setText("LIGHT MODE")
-            }
-        })
-
-        val isNightModeOn = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
-        switch.setChecked(isNightModeOn)
-        if(isNightModeOn){
-            switch.setText("NIGHT MODE")
-        }
-        else{
-            switch.setText("LIGHT MODE")
-        }
+//        //switch button
+//        val switch : SwitchMaterial = findViewById(R.id.nightswitch)
+////        Objects.requireNonNull(getSupportActionBar())?.setTitle("Light-Night MODE SWITCH")
+//
+//        switch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+//            // do something, the isChecked will be
+//            // true if the switch is in the On position
+//            if(isChecked){
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//                buttonView.setText("NIGHT MODE")
+//            }
+//            else{
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//                buttonView.setText("LIGHT MODE")
+//            }
+//        })
+//
+//        val isNightModeOn = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+//        switch.setChecked(isNightModeOn)
+//        if(isNightModeOn){
+//            switch.setText("NIGHT MODE")
+//        }
+//        else{
+//            switch.setText("LIGHT MODE")
+//        }
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -78,5 +85,16 @@ class MainActivity : AppCompatActivity(), InterNotesRVAdapt {
             Toast.makeText(this, "NOTE INSERTED",Toast.LENGTH_SHORT).show()
 
         }
+    }
+
+    fun setUpDrawerLayout(){
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,findViewById(R.id.drawerlayout),R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) return true
+        return super.onOptionsItemSelected(item)
     }
 }
